ddfdad8676d07f83f7212dca5e65a64f8289b859
Grp4webblog <raphael23.bembong@gmail.com>
2021-10-13 23:47:47 +0800

    complete-project
    done the content and made some adjustments in animation and layout

--- projects/digital-portfolio/js/digital-p.js
+++ projects/digital-portfolio/js/digital-p.js
@@ -1,11 +1,11 @@
 // for clicking arrow
-let arrow = document.querySelectorAll('.arrow');
-for (var i = 0; i < arrow.length; i++) {
-  arrow[i].addEventListener("click", (e)=>{
-    let arrowParent = e.target.parentElement.parentElement;
-    arrowParent.classList.toggle("showMenu");
-  });
-}
+// let arrow = document.querySelectorAll('.arrow');
+// for (var i = 0; i < arrow.length; i++) {
+//   arrow[i].addEventListener("click", (e)=>{
+//     let arrowParent = e.target.parentElement.parentElement;
+//     arrowParent.classList.toggle("showMenu");
+//   });
+// }
 
 // for toggling sidebar
 let sidebar = document.querySelector(".sidebar");
@@ -29,8 +29,7 @@ for (var i = 0; i < trivia.length; i++) {
   trivia[i].addEventListener("click", (e)=>{
     e.target.style.opacity = 0;
     e.target.style.pointerEvents = "none";
-    e.target.nextElementSibling.style.opacity = 1;
-    e.target.nextElementSibling.childNodes[3].style.opacity = 0.7;
+    e.target.nextElementSibling.classList.toggle("shown");
     // console.log(e.target.nextElementSibling.childNodes[3]);
   });
 }
